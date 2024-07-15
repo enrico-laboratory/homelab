14318d8d7c37e8a7aeabcad367773c0efdb96dd8
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2024-07-15 11:38:22 +0200

    router playbooks

--- ansible/playbooks/router/firewall.sh
+++ ansible/playbooks/router/firewall.sh
@@ -5,3 +5,6 @@ iptables -F
 
 # Creating NAT
 iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o ens16f0 -j SNAT --to-source 192.168.2.103
+
+# Forwarding dns
+# iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 192.168.100.52
